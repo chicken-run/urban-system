428d64b7e6fcd2b87ab3680e4cd38bcd198c85f0
Texycon <alex.moore.uk@gmail.com>
2016-05-10 00:11:44 +0100

    Task 2 CHANGED Controller to use dependency injection (already set up ApplicationDependencyContainer.cs) ADDED unit test for PatientsController.Get()
    Signed-off-by: Texycon <alex.moore.uk@gmail.com>

--- RestApi.UnitTests/GetPatientTests.cs
+++ RestApi.UnitTests/GetPatientTests.cs
@@ -1,9 +1,70 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using RestApi.Controllers;
+using RestApi.Interfaces;
+using RestApi.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
 
 namespace RestApi.UnitTests
 {
     [TestFixture]
     public class GetPatientTests
     {
+        [Test]
+        public void PatientsController_Get_passesId()
+        {
+            // Arrange
+            var input = 5;
+            var expectedPatient = new Patient()
+            {
+                DateOfBirth = DateTime.UtcNow.Date.AddYears(-25),
+                FirstName = "Felix",
+                LastName = "Milton",
+                NhsNumber = "0268GD54",
+                PatientId = input
+            };
+            var expectedEpisode = new Episode()
+            {
+                PatientId = expectedPatient.PatientId,
+                EpisodeId = 7,
+                Diagnosis = "Concussion",
+                AdmissionDate = DateTime.UtcNow.AddDays(-17),
+                DischargeDate = DateTime.UtcNow.AddDays(-13),
+            };
+            var notToNeReturnedEpisode = new Episode()
+            {
+                PatientId = expectedPatient.PatientId + 1,
+                EpisodeId = 11,
+                Diagnosis = "Fractured Spine",
+                AdmissionDate = DateTime.UtcNow.AddDays(-29),
+                DischargeDate = DateTime.UtcNow.AddDays(-23),
+            };
+
+            var mockPatients = new Mock<IDbSet<Patient>>();
+            mockPatients.Setup(p => p.Find(It.Is<int>(i => input.Equals(i)))).Returns(expectedPatient);
+
+            var episodes = new InMemoryDbSet<Episode>();
+            episodes.Add(expectedEpisode);
+            episodes.Add(notToNeReturnedEpisode);
+            
+            // Mocked Dependency
+            var mockedDbContext = new Mock<IDatabaseContext>();
+            mockedDbContext.Setup(c => c.Patients).Returns(mockPatients.Object);
+            mockedDbContext.Setup(c => c.Episodes).Returns(episodes);
+
+            var objectUnderTest = new PatientsController(mockedDbContext.Object);
+
+            // Act
+            var actual = objectUnderTest.Get(input);
+
+            // Assert
+            mockPatients.VerifyAll();
+            // Not asserting every property - don't want to test Moq functionality 
+            Assert.AreEqual(expectedPatient.PatientId, actual.PatientId);
+            Assert.AreEqual(1, actual.Episodes.Count());
+            Assert.AreEqual(expectedEpisode.PatientId, actual.Episodes.First().PatientId);
+        }
     }
 }
--- RestApi/Controllers/PatientsController.cs
+++ RestApi/Controllers/PatientsController.cs
@@ -8,14 +8,19 @@ namespace RestApi.Controllers
 {
     public class PatientsController : ApiController
     {
-        private readonly PatientContext _patientContext = new PatientContext();
+        private readonly IDatabaseContext _iDatabaseContext;
+
+        public PatientsController(IDatabaseContext iDatabaseContext)
+        {
+            _iDatabaseContext = iDatabaseContext;
+        }
 
         [HttpGet]
         public Patient Get(int patientId)
         {
-            var requestedPatient = _patientContext.Patients.Find(patientId);
+            var requestedPatient = _iDatabaseContext.Patients.Find(patientId);
 
-            requestedPatient.Episodes = _patientContext.Episodes.Where(e => e.PatientId == requestedPatient.PatientId);
+            requestedPatient.Episodes = _iDatabaseContext.Episodes.Where(e => e.PatientId == requestedPatient.PatientId);
 
             return requestedPatient;
         }
