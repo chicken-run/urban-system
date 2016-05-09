94b6f54d7bf969b565ec559c13c484a73c438906
Texycon <alex.moore.uk@gmail.com>
2016-05-09 22:00:57 +0100

    Original
    Signed-off-by: Texycon <alex.moore.uk@gmail.com>

---
+++ RestApi/Controllers/PatientsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Http;
+using RestApi.Interfaces;
+using RestApi.Models;
+
+namespace RestApi.Controllers
+{
+    public class PatientsController : ApiController
+    {
+        [HttpGet]
+        public Patient Get(int patientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
