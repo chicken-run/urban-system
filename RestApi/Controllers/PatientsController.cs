3b6b86e2bdc421c4a6a44c460a5b0b630b84b0a6
Texycon <alex.moore.uk@gmail.com>
2016-05-09 22:37:45 +0100

    Task 1, bare minimum CHANGED PatientsController - implemented method
    Signed-off-by: Texycon <alex.moore.uk@gmail.com>

--- RestApi/Controllers/PatientsController.cs
+++ RestApi/Controllers/PatientsController.cs
@@ -2,15 +2,22 @@
 using System.Web.Http;
 using RestApi.Interfaces;
 using RestApi.Models;
+using System.Linq;
 
 namespace RestApi.Controllers
 {
     public class PatientsController : ApiController
     {
+        private readonly PatientContext _patientContext = new PatientContext();
+
         [HttpGet]
         public Patient Get(int patientId)
         {
-            throw new NotImplementedException();
+            var requestedPatient = _patientContext.Patients.Find(patientId);
+
+            requestedPatient.Episodes = _patientContext.Episodes.Where(e => e.PatientId == requestedPatient.PatientId);
+
+            return requestedPatient;
         }
     }
 }
